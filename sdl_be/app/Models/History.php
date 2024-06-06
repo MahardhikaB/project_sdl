1617791d70d55496defeb3c58d73cde765d2dc2e
Mahardhika Bredy Dwi Gagah Santoso <bredymahardhika@gmail.com>
2024-06-06 08:41:01 +0700

    History

--- sdl_be/app/Models/History.php
+++ sdl_be/app/Models/History.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class History extends Model
 {
@@ -11,11 +12,17 @@ class History extends Model
 
     protected $table = 'histories';
     protected $fillable = ['user_id', 'image'];
-    protected $append = ['image'];
+    protected $append = ['image', 'created_at'];
 
     public function getImageAttribute()
     {
         return asset('storage/' . $this->attributes['image']);
+    }
+
+    public function getCreatedAtAttribute()
+    {
+        $createdAt = Carbon::parse($this->attributes['created_at']);
+        return $createdAt->format('d-m-Y H:i:s'); // dd-mm-yyyy hh:mm:ss format
     }
 
     public function user()
